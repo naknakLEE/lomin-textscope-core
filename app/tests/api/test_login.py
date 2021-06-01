660a1e6d64dfa1125d1994f22327cc3b4c3f72df
Shinuk Yi <wook3024@gmail.com>
2021-06-01 18:02:55 +0900

    Add login test

---
+++ app/tests/api/test_login.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from fastapi.testclient import TestClient
+
+from app.common.const import get_settings
+
+
+settings = get_settings()
+fake_user_info = settings.FAKE_INFORMATION
+
+def test_get_access_token(client: TestClient) -> None:
+    login_data = {
+        "username": fake_user_info["username"],
+        "password": fake_user_info["password"]
+    }
+    response = client.post(f"/auth/token", data=login_data)
+    tokens = response.json()
+    assert response.status_code == 200
+    assert "access_token" in tokens
+    assert "token_type" in tokens
+    assert tokens["access_token"]
+    assert tokens["token_type"] == "bearer"
+
+
+def test_use_access_token(
+    client: TestClient, superuser_token: Dict[str, str]
+) -> None:
+    response = client.get(f"/users/me?token={superuser_token}")
+    result = response.json()
+    print("\n\n\n", result)
+    assert response.status_code == 200
+    assert "username" in result
+    assert "email" in result
+    assert "full_name" in result
---
+++ app/tests/conftest.py
@@ -0,0 +1,86 @@
+from typing import Dict
+
+import asyncio
+import os
+from os import path
+from typing import List
+import sys
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+sys.path.append("/workspace")
+from app.database.schema import Users
+from app.main import create_app
+from app.database.connection import Base, db
+from app.models import UserToken
+from app.routes.auth import create_access_token
+from app.tests.utils.user import authentication_token_from_email
+from app.tests.utils.utils import get_superuser_token_headers
+from app.common.const import get_settings
+
+
+settings = get_settings()
+fake_user_info = settings.FAKE_INFORMATION
+
+
+@pytest.fixture(scope="session")
+def get_db():
+    db_session = db._session()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
+
+
+@pytest.fixture(scope="session")
+def app():
+    os.environ["API_ENV"] = "test"
+    return create_app()
+
+
+@pytest.fixture(scope="session")
+def client(app):
+    # Users.metadata.create_all(db()._engine)
+    Base.metadata.create_all(db._engine)
+    return TestClient(app=app)
+
+@pytest.fixture(scope="module")
+def superuser_token(client: TestClient) -> Dict[str, str]:
+    return get_superuser_token_headers(client)
+
+
+@pytest.fixture(scope="module")
+def normal_user_token_headers(client: TestClient, get_db: Session) -> Dict[str, str]:
+    return authentication_token_from_email(
+        client=client, email=fake_user_info["email"], db=get_db
+    )
+
+
+# @pytest.fixture(scope="function", autouse=True)
+# def session():
+#     sess = next(db.session())
+#     yield sess
+#     clear_all_table_data(
+#         session=sess,
+#         metadata=Base.metadata,
+#         except_tables=[]
+#     )
+#     sess.rollback()
+
+# @pytest.fixture(scope="function")
+# def login(session):
+#     db_user = Users.create(session=session, email="shinuk@example.com", pw="123456")
+#     session.commit()
+#     access_token = create_access_token(data=UserToken.from_orm(db_user).dict(exclude={'pw', 'marketing_agree'}),)
+#     return dict(Authorization=f"Bearer {access_token}")
+
+
+# def clear_all_table_data(session: Session, metadata, except_tables: List[str] = None):
+#     session.excute("SET FOREIGN_KEY_CHECKS = 0;")
+#     for table in metadata.sorted_tables:
+#         if table.name not in except_tables:
+#             session.execute(table.delete())
+#     session.execute("SET FOREIGN_KEY_CHECKS = 1;")
+#     session.commit()
