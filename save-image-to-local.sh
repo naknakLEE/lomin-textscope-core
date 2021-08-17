d1e8ad04ee414812e3727da0561708438695fc45
Shinuk Yi <wook3024@gmail.com>
2021-08-17 14:35:18 +0900

    Add shell script for save image to local

---
+++ save-image-to-local.sh
@@ -0,0 +1,16 @@
+image_repository_set="docker.lomin.ai/ts-web:0.0.1 docker.lomin.ai/ts-gpu-serving:0.0.1 docker.lomin.ai/ts-pp:0.0.1 docker.lomin.ai/ts-wrapper:0.0.1 nginx:latest nginx/nginx-prometheus-exporter:0.9.0 nvcr.io/nvidia/k8s/dcgm-exporter:2.0.13-2.1.2-ubuntu18.04 prom/node-exporter prom/prometheus grafana/grafana prom/mysqld-exporter mysql:8.0.25"
+image_name_mapping_set="ts-web ts-gpu-serving docker.lomin.ai/ts-pp:0.0.1 docker.lomin.ai/ts-wrapper:0.0.1 nginx:latest nginx/nginx-prometheus-exporter:0.9.0 nvcr.io/nvidia/k8s/dcgm-exporter:2.0.13-2.1.2-ubuntu18.04 prom/node-exporter prom/prometheus grafana/grafana prom/mysqld-exporter mysql:8.0.25"
+build_folder_name="build-folder"
+saved_foler_name="saved-docker-images"
+saved_dir="${build_folder_name}/${saved_foler_name}"
+
+mkdir -p saved_dir
+echo "start docker image download process!"
+for image in ${image_repository_set}
+do
+    output_name=$(echo ${image} | awk -F/ '{print $NF}')
+    echo ${image} 
+    echo ${output_name}
+    docker save -o ${saved_dir}/${output_name} ${image}
+done
+echo "docker image download complete!"
