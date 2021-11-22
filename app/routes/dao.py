69bee130ac79acef6343c6be8f322986f843e876
inhak-lomin <ih.lee@lomin.ai>
2021-11-22 17:27:35 +0000

    ADD: dao 추가

--- app/routes/dao.py
+++ app/routes/dao.py
@@ -4,7 +4,7 @@ from app.common.const import get_settings
 from app.database.connection import db
 from app.database import query
 from app.utils.logging import logger
-from app.database.schema import Image, Dataset
+from app.database.schema import Image, Dataset, Model
 from sqlalchemy.orm import Session
 import json
 
@@ -29,12 +29,30 @@ def insert_inference_result(data: Dict = Body(...), db: Session = Depends(db.ses
     return query.insert_inference_result(db, **data)
 
 @router.post("/create/image")
-def insert_inference_result(data: Dict = Body(...),  db: Session = Depends(db.session)):
+def insert_image(data: Dict = Body(...),  db: Session = Depends(db.session)):
     logger.info(f"create image data: {data}")
     return Image.create(db, **data)
 
 @router.post("/create/dataset")
-def insert_inference_result(data: Dict = Body(...),  db: Session = Depends(db.session)):
+def insert_dataset(data: Dict = Body(...),  db: Session = Depends(db.session)):
     logger.info(f"create dataset data: {data}")
     return Dataset.create(db, **data)
 
+@router.post("/create/model")
+def insert_model(data: Dict = Body(...),  db: Session = Depends(db.session)):
+    logger.info(f"create model data: {data}")
+    return Model.create(db, **data)
+
+@router.post("/insert/inference_img_path")
+def insert_img_path(data: Dict = Body(...),  db: Session = Depends(db.session)):
+    logger.info(f"insert infernece_img_path: {data}")
+    return Inference.update(db, **data)
+
+@router.get("/select/category_pkey")
+def select_category_pkey(dataset_id: str,  db: Session = Depends(db.session)):
+    return query.select_category_pkey(db, dataset_id = dataset_id)
+
+
+# @router.get("/select/inference_image")
+# def select_inference_image(task_id: str,  db: Session = Depends(db.session)):
+#     return query.select_inference_image(db, task_id = task_id)
