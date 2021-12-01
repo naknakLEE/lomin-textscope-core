db6a2c17694921f560778fd5a268127d59b96d34
Shinuk Yi <sw.lee@lomin.ai>
2021-12-02 00:42:04 +0900

    minor update

--- app/routes/inference.py
+++ app/routes/inference.py
@@ -1,22 +1,18 @@
 from httpx import AsyncClient
 
 from datetime import datetime
-from typing import Dict, Tuple, Optional, List
+from typing import Dict
 from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
 from app.schemas import inference_responses
 from app.utils.utils import set_json_response, get_pp_api_name
-from app.common.const import get_settings
 from app.utils.logging import logger
+from app.wrapper import pp, pipeline, settings
 
 
-settings = get_settings()
 router = APIRouter()
-
-model_server_url = f"http://{settings.MULTIPLE_GPU_LOAD_BALANCING_NGINX_IP_ADDR}:{settings.MULTIPLE_GPU_LOAD_BALANCING_NGINX_IP_PORT}"
-pp_server_url = f"http://{settings.PP_IP_ADDR}:{settings.PP_IP_PORT}"
 
 
 """
@@ -24,152 +20,6 @@ pp_server_url = f"http://{settings.PP_IP_ADDR}:{settings.PP_IP_PORT}"
 입력 데이터: 토큰, ocr에 사용할 파일 <br/>
 응답 데이터: 상태 코드, 최소 퀄리티 보장 여부, 신뢰도, 문서 타입, ocr결과(문서에 따라 다른 결과 반환)
 """
-
-
-async def classification_inference(client: AsyncClient, inputs: Dict) -> Tuple[int, Dict]:
-    classification_response = await client.post(
-        f"{model_server_url}/classification",
-        json=inputs,
-        timeout=settings.TIMEOUT_SECOND,
-        headers = {"User-Agent": "textscope core"}
-    )
-    classification_result = classification_response.json()
-    return (classification_response.status_code, classification_result)
-
-
-async def detection_inference(
-    client: AsyncClient, 
-    inputs: Dict, 
-    doc_type: str, 
-    hint: Optional[Dict] = None
-) -> Tuple[int, Dict]:
-    # TODO: hint 사용 가능하도록 구성
-    detection_response = await client.post(
-        f"{model_server_url}/detection",
-        json=inputs,
-        timeout=settings.TIMEOUT_SECOND,
-        headers = {"User-Agent": "textscope core"}
-    )
-    detection_result = detection_response.json()
-    return (detection_response.status_code, detection_result)
-
-
-async def recognition_inference(
-    client: AsyncClient, 
-    detection_result: Dict, 
-    inputs: Dict
-) -> Tuple[int, Dict]:
-    recognition_inputs = dict(
-        valid_boxes=detection_result.get("boxes", []),
-        classes=detection_result.get("classes", []),
-        image_path=inputs["image_path"],
-        page=inputs.get("page"),
-    )
-    recognition_response = await client.post(
-        f"{model_server_url}/recognition",
-        json=recognition_inputs,
-        timeout=settings.TIMEOUT_SECOND,
-        headers = {"User-Agent": "textscope core"}
-    )
-    recognition_result = recognition_response.json()
-    return (recognition_response.status_code, recognition_result)
-
-
-# TODO: recify 90 추가 필요
-async def ocr_inference_pipeline(
-    client: AsyncClient, 
-    inputs: Dict, 
-    response_log: Dict,
-) -> Tuple[int, Dict, Dict]:
-    inference_start_time = datetime.now()
-    response_log.update(inference_request_start_time=inference_start_time.strftime('%Y-%m-%d %H:%M:%S'))
-    
-    _, cls_result = await classification_inference(client, inputs)
-    response_log.update(cls_result.get("response_log", {}))
-
-    _, det_result = await detection_inference(client,inputs, cls_result.get("doc_type", None))
-    response_log.update(det_result.get("response_log", {}))
-    
-    rec_status_code, rec_result = await recognition_inference(client, det_result, inputs)
-    response_log.update(rec_result.get("response_log", {}))
-    inference_end_time = datetime.now()
-    logger.info(f"Inference time: {str((inference_end_time - inference_start_time).total_seconds())}")
-    response_log.update(dict(
-        inference_request_end_time=inference_end_time.strftime('%Y-%m-%d %H:%M:%S'),
-        inference_request_time=inference_end_time-inference_start_time,
-    ))
-    converter_status_code, texts = await ocr_convert_preds_to_texts(client, rec_result.get("rec_preds", []))
-    response = dict(
-        class_score=cls_result.get("score", 0.0),
-        scores=det_result.get("scores", []),
-        boxes=det_result.get("boxes", []),
-        classes=det_result.get("classes", []),
-        image_height=det_result.get("image_height"),
-        image_width=det_result.get("image_width"),
-        doc_type=inputs["doc_type"],
-        id_type=det_result["id_type"],
-        rec_preds=rec_result.get("rec_preds", []),
-        texts=texts,
-    )
-    return (rec_status_code, response, response_log)
-
-
-async def ocr_inference(
-    client: AsyncClient, 
-    inputs: Dict, 
-    response_log: Dict,
-) -> Tuple[int, Dict, Dict]:
-    inference_start_time = datetime.now()
-    model_server_response = await client.post(
-        f"{model_server_url}/ocr",
-        json=inputs,
-        timeout=settings.TIMEOUT_SECOND,
-        headers = {"User-Agent": "textscope core"}
-    )
-    inference_end_time = datetime.now()
-    logger.info(f"Inference time: {str((inference_end_time - inference_start_time).total_seconds())}")
-    response_log.update(dict(
-        inference_request_start_time=inference_start_time.strftime('%Y-%m-%d %H:%M:%S'),
-        inference_request_end_time=inference_end_time.strftime('%Y-%m-%d %H:%M:%S'),
-        inference_request_time=inference_end_time-inference_start_time,
-    ))
-    return (model_server_response.status_code, model_server_response.json(), response_log)
-
-
-async def ocr_post_processing(
-    client: AsyncClient, 
-    inference_results: Dict, 
-    post_processing_type: str, 
-    response_log: Dict, 
-    request_id: str,
-) -> Tuple[int, Dict, Dict]:
-    post_processing_start_time = datetime.now()
-    response_log.update(post_processing_start_time=post_processing_start_time.strftime('%Y-%m-%d %H:%M:%S'))
-    inference_results["img_size"] = inference_results["image_height"], inference_results["image_width"]
-    pp_response = await client.post(
-        f"{pp_server_url}/post_processing/{post_processing_type}",
-        json=inference_results,
-        timeout=settings.TIMEOUT_SECOND,
-    )
-    post_processing_end_time = datetime.now()
-    response_log.update(dict(
-        post_processing_end_time=post_processing_end_time.strftime('%Y-%m-do%d %H:%M:%S'),
-        post_processing_time=post_processing_end_time-post_processing_start_time,
-    ))
-    return (pp_response.status_code, pp_response.json(), response_log)
-
-
-async def ocr_convert_preds_to_texts(client: AsyncClient, rec_preds: List, id_type: str = "") -> Tuple[int, Dict]:
-    request_data = dict(
-        rec_preds=rec_preds,
-        id_type="",
-    )
-    convert_response = await client.post(
-        f"{pp_server_url}/convert/recognition_to_text",
-        json=jsonable_encoder(request_data),
-        timeout=settings.TIMEOUT_SECOND,
-    )
-    return (convert_response.status_code, convert_response.json())
 
 
 @router.post("/ocr", status_code=200, responses=inference_responses)
@@ -191,13 +41,15 @@ async def ocr(inputs: Dict = Body(...)) -> Dict:
     async with AsyncClient() as client:
         # ocr inference
         if settings.USE_OCR_PIPELINE:
-            status_code, inference_results, response_log = await ocr_inference_pipeline(
+            # TODO: sequence_type을 wrapper에서 받도록 수정
+            status_code, inference_results, response_log = await pipeline.multiple_model_inference(
                 client=client, 
                 inputs=inputs,
+                sequence_type="kv",
                 response_log=response_log,
             )
         else:
-            status_code, inference_results, response_log = await ocr_inference(
+            status_code, inference_results, response_log = await pipeline.single_model_inference(
                 client=client, 
                 inputs=inputs,
                 response_log=response_log,
@@ -211,7 +63,7 @@ async def ocr(inputs: Dict = Body(...)) -> Dict:
                 inference_results["doc_type"] = inputs.get("test_class")
         post_processing_type = get_pp_api_name(inference_results.get("doc_type"))
         if post_processing_type is not None and len(inference_results["rec_preds"]) > 0:
-            status_code, post_processing_results, response_log = await ocr_post_processing(
+            status_code, post_processing_results, response_log = await pp.ocr_post_processing(
                 client=client, 
                 request_id=request_id,
                 response_log=response_log, 
@@ -225,7 +77,7 @@ async def ocr(inputs: Dict = Body(...)) -> Dict:
 
         # convert preds to texts
         if convert_preds_to_texts is not None:
-            status_code, texts = await ocr_convert_preds_to_texts(
+            status_code, texts = await pp.convert_preds_to_texts(
                 client=client, 
                 rec_preds=inference_results.get("rec_preds", []),
             )
