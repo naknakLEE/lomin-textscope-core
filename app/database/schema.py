19d1859f6b6897a31fb9305cd823b853e6382236
inhak-lomin <94091077+inhak-lomin@users.noreply.github.com>
2021-12-02 15:19:45 +0900

    [MODIFY] DB 수정 및 코드 리팩토링  (#29)
    * MODIFY: 불필요한 코드 제거
    
    * FIx: 오타 수정
    
    * MODIFY: pkey renaming

--- app/database/schema.py
+++ app/database/schema.py
@@ -42,185 +42,7 @@ def verify_password(plain_password, hashed_password) -> bool:
 def get_password_hash(password) -> str:
     return pwd_context.hash(password)
 
-
-class StatusEnum(enum.Enum):
-    active = 1
-    inactive = 2
-    disabled = 3
-
-
-class BaseMixin:
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, nullable=False, default=func.current_timestamp())
-
-    def all_columns(self) -> List:
-        return [
-            c
-            for c in self.__table__.columns
-            if c.primary_key is False and c.name != "created_at"
-        ]
-
-    @classmethod
-    def get(cls, session: Session = None, **kwargs) -> Optional[ModelType]:
-        sess = next(db.session()) if not session else session
-
-        query = sess.query(cls)
-        for key, val in kwargs.items():
-            col = getattr(cls, key)
-            query = query.filter(col == val)
-
-        if sess is None:
-            sess.close()
-        # if query.count() > 1:
-        #     raise Exception("Only one row is supposed to be returned, but got more than one.")
-        return query.first()
-
-    @classmethod
-    def get_multi(
-        cls, session: Session, skip: int = 0, limit: int = 100
-    ) -> Optional[ModelType]:
-        query = session.query(cls).offset(skip).limit(limit)
-        return query.all()
-
-    @classmethod
-    def get_by_email(cls, session: Session, email: EmailStr) -> Optional[ModelType]:
-        query = session.query(cls).filter(cls.email == email)
-        return query.first()
-
-    @classmethod
-    def remove(cls, session: Session, email: EmailStr) -> ModelType:
-        obj = session.query(cls).filter(cls.email == email).delete()
-        session.flush()
-        session.commit()
-        return obj
-
-    @classmethod
-    def update(cls, session: Session, *, db_obj: User, obj_in: UserInDB) -> Any:
-        current_user_email = db_obj.email
-        obj_in.hashed_password = get_password_hash(obj_in.password)
-        obj_data = jsonable_encoder(db_obj)
-        user = session.query(cls).filter(cls.email == current_user_email).first()
-        for field in obj_data:
-            field_value = getattr(obj_in, field) if hasattr(obj_in, field) else None
-            if field_value is not None:
-                setattr(user, field, field_value)
-        session.flush()
-        session.commit()
-        return user
-
-    @classmethod
-    def create(
-        cls, session: Session, auto_commit=True, **kwargs
-    ) -> Optional[ModelType]:
-        is_exist = cls.get_by_email(session, email=kwargs["email"])
-        if is_exist:
-            return "This user already exist"
-        kwargs["hashed_password"] = get_password_hash(kwargs["password"])
-
-        obj = cls()
-        for col in obj.all_columns():
-            col_name = col.name
-            if col_name in kwargs:
-                setattr(obj, col_name, kwargs.get(col_name))
-        session.add(obj)
-        session.flush()
-        if auto_commit:
-            session.commit()
-        # session.refresh(obj)
-        return obj
-
-    @classmethod
-    def create_log(
-        cls, session: Session, auto_commit=True, **kwargs
-    ) -> Optional[ModelType]:
-        obj = cls()
-        for col in obj.all_columns():
-            col_name = col.name
-            if col_name in kwargs:
-                setattr(obj, col_name, kwargs.get(col_name))
-        session.add(obj)
-        session.flush()
-        if auto_commit:
-            session.commit()
-        # session.refresh(obj)
-        return obj
-
-    @classmethod
-    def authenticate(
-        cls, get_db: Session, *, email: str, password: str
-    ) -> Optional[User]:
-        user = cls.get_by_email(get_db, email=email)
-        if not user:
-            return None
-        if not verify_password(password, user.hashed_password):
-            return None
-        return user
-
-    @classmethod
-    def get_usage_count(
-        cls,
-        session: Session,
-        email: EmailStr = None,
-        start_time: datetime = None,
-        end_time: datetime = None,
-    ) -> Optional[ModelType]:
-        if email is not None:
-            query = (
-                session.query(func.count(cls.status_code))
-                .filter(cls.email == email)
-                .group_by(cls.status_code)
-            )
-        else:
-            query = session.query(func.count(cls.email))
-
-        if start_time is not None and end_time is not None:
-            query = query.filter(
-                and_(cls.created_at <= end_time, cls.created_at >= start_time)
-            )
-        elif start_time is not None:
-            query = query.filter(cls.created_at >= start_time)
-        elif end_time is not None:
-            query = query.filter(cls.created_at <= end_time)
-
-        success_response = query.filter(
-            and_(cls.status_code < 300, cls.status_code >= 200)
-        )
-        failed_response = query.filter(
-            or_(cls.status_code >= 300, cls.status_code < 200)
-        )
-        return {
-            "success_response": success_response.all(),
-            "failed_response": failed_response.all(),
-        }
-
-    @classmethod
-    def get_usage(
-        cls,
-        session: Session,
-        email: EmailStr = None,
-        skip: int = 0,
-        limit: int = 100,
-        start_time: datetime = None,
-        end_time: datetime = None,
-    ) -> Optional[ModelType]:
-        query = session.query(cls)
-        if start_time is not None and end_time is not None:
-            query = query.filter(
-                and_(cls.created_at <= end_time, cls.created_at >= start_time)
-            )
-        elif start_time is not None:
-            query = query.filter(cls.created_at >= start_time)
-        elif end_time is not None:
-            query = query.filter(cls.created_at <= end_time)
-
-        if email is not None:
-            query = query.filter(cls.email == email)
-        else:
-            query = query.offset(skip).limit(limit)
-
-        return query.all()
-
-class WooriBaseMixin:
+class HeungkukBaseMixin:
     def all_columns(self) -> List:
         return [
             c
@@ -293,10 +115,10 @@ class WooriBaseMixin:
         # session.refresh(obj)
         return obj
 
-class Category(Base):
+class Category(Base, HeungkukBaseMixin):
     __tablename__ = 'category'
 
-    category_peky = Column(Integer, primary_key=True)
+    pkey = Column(Integer, primary_key=True)
     category_name_kr = Column(String(50), comment='카테고리명_한글')
     category_name_en = Column(String(50), comment='카테고리명_영문')
     category_code = Column(String(50), comment='고유서식코드')
@@ -304,10 +126,10 @@ class Category(Base):
     task = relationship('Task', back_populates='category')
 
 
-class Image(Base):
+class Image(Base, HeungkukBaseMixin):
     __tablename__ = 'image'
 
-    image_pkey = Column(Integer, primary_key=True)
+    pkey = Column(Integer, primary_key=True)
     image_id = Column(String(50), nullable=False)
     image_path = Column(String(50))
     image_description = Column(String(50))
@@ -316,13 +138,13 @@ class Image(Base):
     task = relationship('Task', back_populates='image')
 
 
-class Task(Base):
+class Task(Base, HeungkukBaseMixin):
     __tablename__ = 'task'
 
-    task_pkey = Column(Integer, primary_key=True)
+    pkey = Column(Integer, primary_key=True)
     task_id = Column(String(50), nullable=False)
-    image_pkey = Column(ForeignKey('image.image_pkey'))
-    category_peky = Column(ForeignKey('category.category_peky'))
+    image_pkey = Column(ForeignKey('image.pkey'))
+    category_pkey = Column(ForeignKey('category.pkey'))
     create_datetime = Column(DateTime)
 
     category = relationship('Category', back_populates='task')
@@ -330,12 +152,12 @@ class Task(Base):
     inference = relationship('Inference', back_populates='task')
 
 
-class Inference(Base):
+class Inference(Base, HeungkukBaseMixin):
     __tablename__ = 'inference'
 
-    inference_pkey = Column(Integer, primary_key=True)
+    pkey = Column(Integer, primary_key=True)
     inference_id = Column(String(50))
-    task_pkey = Column(ForeignKey('task.task_pkey'))
+    task_pkey = Column(ForeignKey('task.pkey'))
     inference_type = Column(String(5), comment="['cls', 'kv', 'gocr', 'reco']")
     inference_img_path = Column(String(50))
     inference_result = Column(JSON)
@@ -348,30 +170,28 @@ class Inference(Base):
 
 
 
-
-
 '''
 
 -- image Table Create SQL
 CREATE TABLE image
 (
-    image_pkey           integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
+    pkey                 integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
     image_id             varchar(50)    NOT NULL, 
     image_path           varchar(50)    NULL, 
     image_description    varchar(50)    NULL, 
     create_datetime      timestamp      NULL, 
-     PRIMARY KEY (image_pkey)
+     PRIMARY KEY (pkey)
 );
 
 
 -- category Table Create SQL
 CREATE TABLE category
 (
-    category_peky       integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
+    pkey                integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
     category_name_kr    varchar(50)    NULL, 
     category_name_en    varchar(50)    NULL, 
     category_code       varchar(50)    NULL, 
-     PRIMARY KEY (category_peky)
+     PRIMARY KEY (pkey)
 );
 
 COMMENT ON COLUMN category.category_name_kr IS '카테고리명_한글';
@@ -384,27 +204,27 @@ COMMENT ON COLUMN category.category_code IS '고유서식코드';
 -- task Table Create SQL
 CREATE TABLE task
 (
-    task_pkey          integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
+    pkey               integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
     task_id            varchar(50)    NOT NULL, 
     image_pkey         integer        NULL, 
-    category_peky      integer        NULL, 
+    category_pkey      integer        NULL, 
     create_datetime    timestamp      NULL, 
-     PRIMARY KEY (task_pkey)
+     PRIMARY KEY (pkey)
 );
 
 ALTER TABLE task
-    ADD CONSTRAINT FK_task_image_pkey_image_image_pkey FOREIGN KEY (image_pkey)
-        REFERENCES image (image_pkey);
+    ADD CONSTRAINT FK_task_image_pkey_image_pkey FOREIGN KEY (image_pkey)
+        REFERENCES image (pkey);
 
 ALTER TABLE task
-    ADD CONSTRAINT FK_task_category_peky_category_category_peky FOREIGN KEY (category_peky)
-        REFERENCES category (category_peky);
+    ADD CONSTRAINT FK_task_category_pkey_category_pkey FOREIGN KEY (category_pkey)
+        REFERENCES category (pkey);
 
 
 -- inference Table Create SQL
 CREATE TABLE inference
 (
-    inference_pkey        integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
+    pkey                  integer        GENERATED BY DEFAULT AS IDENTITY NOT NULL, 
     inference_id          varchar(50)    NULL, 
     task_pkey             integer        NULL, 
     inference_type        varchar(5)     NULL, 
@@ -414,7 +234,7 @@ CREATE TABLE inference
     finsh_datetime        timestamp      NULL, 
     create_datetime       timestamp      NULL, 
     inference_sequence    integer        NULL, 
-     PRIMARY KEY (inference_pkey)
+     PRIMARY KEY (pkey)
 );
 
 COMMENT ON COLUMN inference.inference_type IS '[''cls'', ''kv'', ''gocr'', ''reco'']';
@@ -422,6 +242,7 @@ COMMENT ON COLUMN inference.inference_type IS '[''cls'', ''kv'', ''gocr'', ''rec
 COMMENT ON COLUMN inference.inference_sequence IS 'inference 순서 1->2->3->4';
 
 ALTER TABLE inference
-    ADD CONSTRAINT FK_inference_task_pkey_task_task_pkey FOREIGN KEY (task_pkey)
-        REFERENCES task (task_pkey);
+    ADD CONSTRAINT FK_inference_task_pkey_task_pkey FOREIGN KEY (task_pkey)
+        REFERENCES task (pkey);
+
 '''
