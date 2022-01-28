6d014841324a82e082314196a3332883714cd3f9
Shinuk Yi <sw.lee@lomin.ai>
2022-01-28 16:35:40 +0900

    Remove docker compose up line in init.sh

--- init.sh
+++ init.sh
@@ -14,6 +14,3 @@ docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --parallel
 
 # Generate textscope model service
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml run serving python3 inference_server/generate_textscope_model_service.py --model_service textscope_model_service
-
-# Run textscope
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
