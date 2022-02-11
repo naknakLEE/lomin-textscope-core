9c681b71c1c70b1ee51c311f0943e3f8c5d46c75
Shinuk Yi <sw.lee@lomin.ai>
2022-02-11 17:44:55 +0900

    Remove textscope model generation process in init.sh

--- init.sh
+++ init.sh
@@ -12,5 +12,5 @@ aws s3 cp s3://lomin-model-repository/textscope/.env .env
 # Build docker containers
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --parallel
 
-# Generate textscope model service
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml run serving python3 inference_server/generate_textscope_model_service.py --model_service textscope_model_service
+# Up docker-compose
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
