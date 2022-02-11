b4e79cf11a929846dfa01804da0688c0b124dc66
inhak-lomin <94091077+inhak-lomin@users.noreply.github.com>
2022-02-11 21:09:49 +0900

    Trial을 위한 CircleCI 셋팅 (#91)
    * ADD: circleci only develop PR
    
    * MODIFY: fix port conflict
    
    * MODIFY: remove docker-compose iup
    
    * ADD: 도커 실행 이후 실패 시 대응 코드 추가
    
    * MODIFY: failed 대응 코드 수정
    
    * MODIFY: fail시 대응 코드 수정

--- init.sh
+++ init.sh
@@ -11,6 +11,3 @@ aws s3 cp s3://lomin-model-repository/textscope/.env .env
 
 # Build docker containers
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --parallel
-
-# Up docker-compose
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
