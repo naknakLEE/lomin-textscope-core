2e1baf0c8bc7e2b1aea23855011e99698a79d3da
pilljoongk <pj.kim@lomin.ai>
2022-05-17 15:50:31 +0900

    도커 이미지 save 시 md5 체크섬도 기록

--- assets/save-image-to-local.sh
+++ assets/save-image-to-local.sh
@@ -8,16 +8,15 @@ build_folder_name="${BUILD_FOLDER_PATH}"
 image_repository_list="${IMAGE_REPOSITORY_LIST}"
 saved_dir="${base_path}/build/images/${CUSTOMER}"
 
-echo "docker image download start!"
+echo "docker image save start!"
 mkdir -p ${saved_dir}
+echo "            md5sum                           docker_image_path" > ${saved_dir}/"checksum(md5).txt"
 
-echo "start docker image download process!"
 for image in ${image_repository_list}
 do
     output_name=$(echo ${image} | awk -F/ '{print $NF}' | tr : _)
     echo ${image} '|' ${output_name}
-    # docker save -o ${saved_dir}/${output_name}.tar ${image}# 일반 
-    docker save ${image} | pigz -6 > ${saved_dir}/${output_name}.tar.gz # 압축
-
+    docker save ${image} | pigz -6 > ${saved_dir}/${output_name}.tar.gz
+    echo `md5sum ${saved_dir}/${output_name}.tar.gz` >> ${saved_dir}/"checksum(md5).txt"
 done
-echo "docker image download complete!"
+echo "docker image save complete!"
