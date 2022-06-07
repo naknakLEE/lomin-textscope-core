fed08a167dd3e83f664c82e418576494d98e0cb5
inhak-lomin <ih.lee@lomin.ai>
2022-06-08 00:22:13 +0900

    ADD: 모델 컨테이너 저장 필요 시 스크립트 사용

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -eux
 
 # set enviroments
@@ -49,6 +51,54 @@ do
     echo "Create folder, ${file}"
     mkdir -p ${build_folder_name}/${created_folder_name}/assets/${file}
 done
+
+
+####################################################################vv
+# model을 build폴더로 저장하는 로직
+rm -rf build/textscope/serving/models
+mkdir -p build/textscope/serving/models
+bsn_name_before_split=`cat inference_server/assets/conf/config.yaml | grep model`
+bsn_name=${bsn_name_before_split##*- model:}
+bsn_name=`echo $bsn_name | tr -d ' '`
+echo $bsn_name
+
+model_length=`cat inference_server/assets/conf/model/${bsn_name}.yaml | shyaml get-length resources`
+model_length=$((model_length - 1))
+range=$(seq 0 ${model_length})
+for i in $range # 모델 전체 개수 for 돌면서
+do
+    if cat inference_server/assets/conf/model/${bsn_name}.yaml | shyaml get-value resources.${i}.copy_container; then # copy_container변수명 존재 체크
+        is_copy=`cat inference_server/assets/conf/model/${bsn_name}.yaml | shyaml get-value resources.${i}.copy_container`
+        if [ "$is_copy" = True ] ; then # copy_container가 true면 build 폴더에 copy
+            # copy model
+            model_path=`cat inference_server/assets/conf/model/${bsn_name}.yaml | shyaml get-value resources.${i}.model_path`
+            build_model_path=${model_path#assets/}
+            filename=${build_model_path##*/} # split / get last char
+            folder_path=`echo $build_model_path | rev | cut -d '/' -f2- | rev ` # /로 나누고 마지막(파일명) 제외
+
+            mkdir -p build/textscope/serving/${folder_path}
+            cp inference_server/${model_path} build/textscope/serving/${build_model_path}
+
+            # copy meta data
+            if cat inference_server/assets/conf/model/${bsn_name}.yaml | shyaml get-value resources.${i}.model_metadata; then
+                metedata_path=`cat inference_server/assets/conf/model/${bsn_name}.yaml | shyaml get-value resources.${i}.model_metadata`
+                build_metadata_path=${metedata_path#assets/}
+                metadata_filename=${build_metadata_path##*/} # split / get last char
+                metadata_folder_path=`echo $build_metadata_path | rev | cut -d '/' -f2- | rev ` # /로 나누고 마지막(파일명) 제외
+
+                mkdir -p build/textscope/serving/${metadata_folder_path}
+                cp inference_server/${metedata_path} build/textscope/serving/${build_metadata_path}
+            fi
+        fi
+    else
+        echo "Optional value(copy_container) does not exist. I'll skip that line."
+    fi
+done
+
+# to inference build folder
+mkdir -p build/textscope/serving/models
+cp -r build/textscope/serving/models inference_server/build/textscope/serving/models
+####################################################################vv
 
 docker-compose down
 
