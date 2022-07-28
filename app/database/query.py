3f8044249e5cdc5596b1bb8a9eb766c76dd5451b
inhak-lomin <ih.lee@lomin.ai>
2022-07-29 01:12:37 +0900

    ADD: 인식 되지 않은 class None값으로 추가

--- app/database/query.py
+++ app/database/query.py
@@ -50,6 +50,15 @@ def select_kv_class_info_get_all_multi(session: Session, **kwargs: Dict) -> Unio
         raise CoreCustomException(4101, "모든 key value classes")
     return result
 
+def select_doc_type_kv_class_get_all(session: Session, **kwargs: Dict) -> Union[List[schema.DocTypeKvClass], JSONResponse]:
+    try:
+        result = schema.DocTypeKvClass.get_all(session, **kwargs)
+        if result is None:
+            raise CoreCustomException(2108)
+    except Exception:
+        raise CoreCustomException(4101, "소분류와 kv class 정보")
+    return result
+
 
 
 def select_cls_group_model(session: Session, **kwargs: Dict) -> Union[schema.ClsGroupModel, JSONResponse]:
--- app/routes/document.py
+++ app/routes/document.py
@@ -888,12 +888,35 @@ def get_document_inference_info(
         return select_inspect_result
     select_inspect_result: schema.InspectInfo = select_inspect_result
     
-    # kv에 kv_class_name_kr 한글명 추가
-    kv_class_codes = select_inference_result.inference_result.get("kv").keys()
-    select_kv_class_result = query.select_kv_class_info_get_all_multi(session, kv_class_code=kv_class_codes)
-    for class_code, kv_class_object in zip(kv_class_codes, select_kv_class_result):
-        select_inference_result.inference_result["kv"][class_code]["kv_class_name_kr"] = kv_class_object.kv_class_name_kr
+    if select_inference_result.inference_result.get("kv", None):
+        kv_class_codes = select_inference_result.inference_result.get("kv").keys()
+        
+        # 인식 되지 않은 class None값으로 추가
+        doc_type_idx = select_inference_result.doc_type_idx
+        select_doc_kv_result = query.select_doc_type_kv_class_get_all(session, doc_type_idx=doc_type_idx)
+        maximun_kv_code = []
+        for d in select_doc_kv_result:
+            maximun_kv_code.append(d.kv_class_code)
+            
+        for max_kv_code in maximun_kv_code:
+            if max_kv_code not in kv_class_codes:
+                select_inference_result.inference_result["kv"][max_kv_code] = {
+                    "text": "",
+                    "score": 0,
+                    "class": max_kv_code,
+                    "box": [0,0,0,0],
+                    "merged_count": 0,
+                }
+        
+        
+        # kv에 kv_class_name_kr 한글명 추가
+        select_kv_class_result = query.select_kv_class_info_get_all_multi(session, kv_class_code=kv_class_codes)
+        for class_code, kv_class_object in zip(kv_class_codes, select_kv_class_result):
+            select_inference_result.inference_result["kv"][class_code]["class_name"] = kv_class_object.kv_class_name_kr
 
+        
+        
+        
     
     response = dict(
         document_id=select_inference_result.document_id,
