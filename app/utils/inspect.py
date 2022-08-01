2447de0f10dd11b221e6cf3539d32ef5f6b2ee77
inhak-lomin <ih.lee@lomin.ai>
2022-08-02 01:23:23 +0900

    FIX: inspect accuracy 수정

--- app/utils/inspect.py
+++ app/utils/inspect.py
@@ -1,21 +1,14 @@
 from sqlalchemy.orm import Session
 from app.utils.postprocess import add_unrecognition_kv
-from app.utils.postprocess import modif_class_code, del_class_code
+from app.database import schema
 
 
-def get_inspect_accuracy(session: Session, select_inference_result: dict, inspect_result: dict):
+def get_inspect_accuracy(session: Session, select_inference_result: schema.InferenceInfo, inspect_result: dict):
     # 인식 되지 않은 class None값으로 추가
     
-    # TODO DB 이후 inference_result 수정이 되어 input이 DB inference_result 값과 달라짐
-    # input값과 db의 inference_result값이 같다면 사용안해도 됨.
-    del_class_code(select_inference_result)
-    if select_inference_result.inference_result.get("doc_type", "NONE") in ["FN-OIP", "FN-BP"]:
-        modif_class_code(select_inference_result)
-    
-    
+    select_inference_result, unrecognition_kv = add_unrecognition_kv(session, select_inference_result)
     inference_kv = select_inference_result.inference_result.get("kv")
     inference_kv_count = len(inference_kv)
-    _, unrecognition_kv = add_unrecognition_kv(session, select_inference_result)
     inspect_kv = inspect_result.get("kv")
     
     # 미검출 항목 수정 개수 확인
