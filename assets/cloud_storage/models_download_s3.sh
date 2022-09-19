1bf8a4d3ab2fbac19d7a7cc773fac63c09be50ba
ilwoo-lomin <ilwoo.park@lomin.ai>
2022-09-19 15:46:07 +0900

    서울보증보험_v0.15.0 배포 1. gitmodules (서브모듈 버전관리)최신화 2. SUBMOUDLE UPDATE 3. confing.yaml 서울보증보험용으로 업데이트 4. wrapper에서 dockerfile로 빌드시 poetry install 안되던거 수정

---
+++ assets/cloud_storage/models_download_s3.sh
@@ -0,0 +1,18 @@
+model_config=`cat ../conf/config.yaml | shyaml get-value defaults.2.model`
+model_count=`cat ../conf/model/${model_config}.yaml | shyaml get-length resources`
+model_count=$((${model_count} - 1))
+
+for index in `seq 0 ${model_count}`
+do
+    model_path=`cat ../conf/model/${model_config}.yaml | shyaml get-value resources.${index}.model_path`
+    
+    if test -f ../../${model_path}; then
+        echo "Already exist"
+    else
+        path_depth=`echo ${model_path} | tr -cd '/' | wc -m`
+        model_path_s3=`echo ${model_path} | cut -d '/' -f 1-${path_depth}`
+        echo "[$((${index} + 1))/$((${model_count} + 1))]"
+        aws s3 cp s3://lomin-model-repository/textscope/${model_path_s3}/ ../../${model_path_s3}/ --recursive
+        echo ""
+    fi
+done
