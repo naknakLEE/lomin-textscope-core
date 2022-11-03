e90243db294bccd621346b8b0653337935a3dda0
pilljoongk <pj.kim@lomin.ai>
2022-11-03 19:08:15 +0900

    ADD: more logs to [POST] /api/v1/docx

--- app/routes/base.py
+++ app/routes/base.py
@@ -88,6 +88,9 @@ async def post_upload_document(
     document_data:str = inputs.get("document_data")
     document_path:str = inputs.get("document_path")    
 
+    logger.info("#0-1 start upload document at {}", datetime.now())
+    logger.info("#0-2 document_name: {}, len(document_data): {}, document_path {}", document_name, len(document_data), document_path)
+
     response: dict = dict(resource_id=dict())
     response_log: dict = dict()
     request_datetime = datetime.now()
@@ -104,14 +107,20 @@ async def post_upload_document(
             document_data = base64.b64encode(document_data)    
     
     # 업로드된 파일 포맷(확장자) 확인
+    logger.info("[{}] #1-1 try check support format", datetime.now())
     is_support = is_support_format(document_name)
+    logger.info("[{}] #1-2 end check support format (suport: {})", datetime.now(), is_support)
+    
     # 지원하지 않은 포맷(확장자)일시 "지원하지 않는 파일 형식입니다" Error return
     if is_support is False:
         status_code, error = ErrorResponse.ErrorCode.get(2105)
         return JSONResponse(status_code=status_code, content=jsonable_encoder({"error":error}))
 
     # 유저 정보 확인(Document Info Insert시 유저 정보가 필요하여서 실행함..)
+    logger.info("[{}] #2-1 try select user_email from db", datetime.now())
     select_user_result = query.select_user(session, user_email=user_email)
+    logger.info("[{}] #2-2 end select user_email from db (user_info: {})", datetime.now(), type(select_user_result))
+    
     if isinstance(select_user_result, JSONResponse):
         return select_user_result
     select_user_result: schema.UserInfo = select_user_result
@@ -121,7 +130,9 @@ async def post_upload_document(
     document_id = get_ts_uuid("document")
 
     # 문서 저장(minio or local pc)
+    logger.info("[{}] #3-1 try save or upload documnet", datetime.now())
     save_success, save_path = save_upload_document(document_id, document_name, document_data)
+    logger.info("[{}] #3-2 end save or upload documnet (save_success: {})", datetime.now(), save_success)
 
     # 문서 저장에 실패하였을 경우 "문서 정보를 저장하는 중 에러가 발생했습니다" Error return
     if not save_success:
@@ -130,7 +141,10 @@ async def post_upload_document(
         return JSONResponse(status_code=status_code, content=jsonable_encoder({"error":error}))
 
     # 문서 저장에 성공했을시, document 테이블에 insert        
+    logger.info("[{}] #4-1 try get page count", datetime.now())
     document_pages = get_page_count(document_data, document_name)
+    logger.info("[{}] #4-2 end get page cunt (document_pages: {})", datetime.now(), document_pages)
+    
     logger.info(f"success save document document_id : {document_id}")
     # 기존 document insert에서 document_description, doc_type_idx, document_type 제외
     dao_document_params = {
@@ -140,7 +154,10 @@ async def post_upload_document(
         "document_path": save_path,
         "document_pages": document_pages,
     }
+    
+    logger.info("[{}] #5-1 try insert documnet info to db", datetime.now())
     insert_document_result = query.insert_document(session, **dao_document_params)
+    logger.info("[{}] #5-2 try insert documnet info to db (result: {})", datetime.now(), insert_document_result)
     if isinstance(insert_document_result, JSONResponse):
         return insert_document_result            
 
@@ -166,5 +183,8 @@ async def post_upload_document(
         response_log=response_log,
         document_id = document_id,
     )
+
+    logger.info("#6-1 upload document end at {}", datetime.now())
+    logger.info("#6-2 end document_id: {}", document_id)
 
     return JSONResponse(status_code=200, content=jsonable_encoder(response))
