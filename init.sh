632dd32a1216d5754fb7b296ddcc80939821b34a
inhak-lomin <ih.lee@lomin.ai>
2022-11-25 17:46:04 +0900

    ADD: env 프로젝트에 맞게 download

--- init.sh
+++ init.sh
@@ -4,6 +4,19 @@ set -eux
 set +x
 printf "sudo Password: "
 read PASSWORD
+
+
+
+echo "  1)default"
+echo "  2)kdt2204"
+echo -n "프로젝트 번호를 선택해주세요: "
+read project_name
+
+case $project_name in
+  1) project_name="";;
+  2) project_name="kdt2204";;
+  *) echo "입력값이 잘못 입려되었습니다.";;
+esac
 set -x
 
 create_log_folder() {
@@ -21,7 +34,7 @@ python3 inference_server/assets/cloud_storage/boto3_download_data.py
 python3 assets/cloud_storage/boto3_download_data.py --download_object_name thales
 
 # Download environments
-aws s3 cp s3://lomin-model-repository/textscope/.env .env
+aws s3 cp s3://lomin-model-repository/textscope/${project_name}/.env .env
 
 # Create log folder and change owner
 set +x
