dbd549cec901cb6346974286a0189a0811d126ff
pilljoongk <pj.kim@lomin.ai>
2022-11-29 11:47:17 +0900

    FIX: base api 정리

--- app/routes/base.py
+++ app/routes/base.py
@@ -41,7 +41,7 @@ async def post_auth_token(
     inputs: OAuth2PasswordRequestForm = Depends()
 ) -> JSONResponse:
     """
-    ### [Base]전용 OAuth2.0 토큰 발급
+    ### [Base] OAuth2.0 토큰 발급
     입력받은 Email을 토대로 OAuth2.0 토큰을 생성후 Return
     """
     # AceessToken 만료시간 세팅
@@ -65,24 +65,23 @@ async def post_auth_token(
 async def post_upload_document(
     inputs: Dict = Body(...),
     current_user: UserInfoInModel = Depends(get_current_active_user),
-    session: Session = Depends(db.session),    
+    session: Session = Depends(db.session)
 ) -> JSONResponse:
     """
-    ### [Base]전용 문서 업로드
+    ### [Base] 문서 업로드
     base64encoding된 문서 data와 문서 파일명을 토대로 적재(minio or local)후 DB에 Document Info Insert<br>
     문서 고유 ID(document_id) return
     """
     # 시작 시간 측정
     request_datetime = datetime.now()
 
-    user_email:str    = current_user.email
-    document_name:str = inputs.get("document_name")
-    document_data:str = inputs.get("document_data")
-    retry:bool = inputs.get("retry")
+    user_email:    str    = current_user.email
+    document_id:   str    = inputs.get("document_id")
+    document_name: str    = inputs.get("document_name")
+    document_data: str    = inputs.get("document_data")
     
-    response: dict = dict(resource_id=dict())
+    response: dict = dict()
     response_log: dict = dict()
-    request_datetime = datetime.now()
     
     # 업로드된 파일 포맷(확장자) 확인
     is_support = is_support_format(document_name)
@@ -90,17 +89,18 @@ async def post_upload_document(
     if is_support is False:
         status_code, error = ErrorResponse.ErrorCode.get(2105)
         return JSONResponse(status_code=status_code, content=jsonable_encoder({"error":error}))
-
+    
     # 유저 정보 확인(Document Info Insert시 유저 정보가 필요하여서 실행함..)
     select_user_result = query.select_user(session, user_email=user_email)
     if isinstance(select_user_result, JSONResponse):
         return select_user_result
     select_user_result: schema.UserInfo = select_user_result
-    user_team = select_user_result.user_team        
-
+    user_team = select_user_result.user_team
+    
     # 고유한 document_id 생성 -> DB적재용이 아닌 path를 만들기 위함!
     # 고객사에서 document_id를 지정해서 보내줄 경우 해당 아이디가 minio, DB에 들어가도록 처리
-    document_id = inputs.get("document_id") if inputs.get("document_id") != "" else get_ts_uuid("document")
+    document_id = document_id if document_id else get_ts_uuid("document")
+    logger.info("start upload docx (document_id: {})", document_id)
 
     # 문서 저장에 성공했을시, document 테이블에 insert        
     document_pages, document_name = get_page_count(document_data, document_name)
@@ -119,23 +119,15 @@ async def post_upload_document(
     dao_document_params = {
         "user_email": user_email,
         "user_team": user_team,
+        "document_id":document_id,
         "document_path": save_path,
-        "document_pages": document_pages,
+        "document_pages": document_pages
     }
-    if not retry:
-        dao_document_params["document_id"] = document_id
     
-    # 고객사에서 동일 document_id로 문서 업로드 재시도 케이스 - update 처리 
-    insert_document_result = \
-        query.update_document(session, document_id, **dao_document_params) \
-        if retry \
-        else query.insert_document(session, **dao_document_params)
-    if isinstance(insert_document_result, JSONResponse):
-        dao_document_params["document_id"] = document_id
-        insert_document_result = query.insert_document(session, **dao_document_params)
+    insert_document_result = query.insert_document(session, **dao_document_params)
     if isinstance(insert_document_result, JSONResponse):
         return insert_document_result
-        
+    
     # 종료 시간 측정 
     response_datetime = datetime.now()
     # 걸린 시간 측정 (종료시간 - 시작시간)
@@ -143,11 +135,9 @@ async def post_upload_document(
     
     # response log 생성(시작시간, 종료시간, 걸린시간)
     response_log.update(
-        dict(
         request_datetime=request_datetime,
         response_datetime=response_datetime,
-        elapsed=elapsed,
-        )
+        elapsed=elapsed
     )
     
     # response 객체 생성
@@ -164,30 +154,40 @@ async def post_upload_document(
 @router.post("/docx/delete")
 async def post_delete_document(
     inputs: Dict = Body(...),
+    session: Session = Depends(db.session)
 ) -> JSONResponse:
     """
-    ### [Base]전용 문서 삭제
+    ### [Base] 문서 삭제
     미니오에 저장된 document를 삭제합니다.
     """
     # 시작 시간 측정
     request_datetime = datetime.now()
 
-    document_id:str = inputs.get("document_id")
-    document_name:str = inputs.get("document_name")
+    document_id:   str = inputs.get("document_id")
+    # document_name: str = inputs.get("document_name")
 
-    response: dict = dict(resource_id=dict())
+    response: dict = dict()
     response_log: dict = dict()
     request_datetime = datetime.now()
     
-    # 문서 저장(minio or local pc)
-    delete_success = delete_document(document_id, document_name)
-
-    # 문서 저장에 실패하였을 경우 "문서 정보를 저장하는 중 에러가 발생했습니다" Error return
+    select_document_result = query.select_document(session, document_id=document_id, is_used=True)
+    # document_id에 해당하는 문서가 없을 경우 Error return
+    if isinstance(select_document_result, JSONResponse):
+        return select_document_result
+    select_document_result: schema.DocumentInfo = select_document_result
+    
+    # 문서 삭제(minio or local pc)
+    delete_success = delete_document(document_id, select_document_result.document_path)
+    
+    # is_used True -> False
+    _ = query.update_document(session, document_id=document_id, is_used=False)
+    
+    # 문서 삭제에 실패하였을 경우 "문서를 삭제하는 중 에러가 발생했습니다" Error return
     if not delete_success:
         status_code, error = ErrorResponse.ErrorCode.get(4103)
         error.error_message = error.error_message.format("문서")
         return JSONResponse(status_code=status_code, content=jsonable_encoder({"error":error}))
-
+    
     # 종료 시간 측정 
     response_datetime = datetime.now()
     # 걸린 시간 측정 (종료시간 - 시작시간)
@@ -195,11 +195,9 @@ async def post_delete_document(
     
     # response log 생성(시작시간, 종료시간, 걸린시간)
     response_log.update(
-        dict(
         request_datetime=request_datetime,
         response_datetime=response_datetime,
-        elapsed=elapsed,
-        )
+        elapsed=elapsed
     )
     
     # response 객체 생성
@@ -209,5 +207,5 @@ async def post_delete_document(
         elapsed=elapsed,
         response_log=response_log,
     )
-
+    
     return JSONResponse(status_code=200, content=jsonable_encoder(response))
