52f9c4bdd36b0ea6c2c1324d61ceac57707f8149
ilwoo-lomin <ilwoo.park@lomin.ai>
2023-01-25 18:33:39 +0900

    fixed circleci args

--- circleci/build.sh
+++ circleci/build.sh
@@ -47,7 +47,7 @@ do
         path_depth=`echo ${model_path} | tr -cd '/' | wc -m`
         model_path_s3=`echo ${model_path} | cut -d '/' -f 1-${path_depth}`
         
-        aws s3 cp s3://lomin-model-repository/textscope/${model_path_s3}/ ../inference_server/${model_path_s3}/ --recursive
+        aws s3 cp s3://lomin-model-repository/textscope/${model_path_s3} inference_server/${model_path_s3}/ --recursive
         echo ""
     fi
 done
