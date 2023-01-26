39a482b3d193b4ffdcad4c681c4485c44ee319e1
ilwoo-lomin <ilwoo.park@lomin.ai>
2023-01-26 10:47:25 +0900

    check aws cp

--- circleci/build.sh
+++ circleci/build.sh
@@ -6,6 +6,6 @@ ifconfig
 aws configure list
 cat ~/.aws/config
 
-aws s3 cp s3://lomin-model-repository/textscope/assets/models/gocr/detection/2/kxm-det-220816-changwoo-02-iter=0054999_f1=97.3104-topk=10000-batch=1.ts/ /app/
+aws s3 cp s3://lomin-model-repository/textscope/assets/models/gocr/detection/2/kxm-det-220816-changwoo-02-iter=0054999_f1=97.3104-topk=10000-batch=1.ts app/
 
 cd app && ls -al
