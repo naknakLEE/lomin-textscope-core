f07b0d060e064982006b5a4e5092474b952ff883
ilwoo-lomin <ilwoo.park@lomin.ai>
2023-02-08 13:42:14 +0900

    circleci test용 PR입니다.

---
+++ circleci/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -eux
+
+set +x
+
+PATH="$HOME/.local/bin:$PATH"
+
+########## 1. Prepare Start ##########
+# pip3 install hydra-core python-dotenv alive_progress awscli
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+mkdir -p /home/circleci/.config/ssh && cp ~/.ssh/id_rsa ~/.config/ssh/id_rsa
+
+git submodule update --init --recursive
+########## 1. Prepare End   ##########
+
+
+########## 2. Download Model File Start ##########
+model_config=`cat inference_server/assets/conf/config.yaml | shyaml get-value defaults.2.model`
+if test [[ model_config = 'default' ]];
+then
+    echo "Change Model Config To ${BSN_CODE}"
+    sed -i "s/model: default/model: ${BSN_CODE}/gi" inference_server/assets/conf/config.yaml
+    model_config=${BSN_CODE}
+fi
+
+model_count=`cat inference_server/assets/conf/model/${model_config}.yaml | shyaml get-length resources`
+model_count=$((${model_count} - 1))
+
+aws configure list
+
+for index in `seq 0 ${model_count}`
+do
+    model_name=`cat inference_server/assets/conf/model/${model_config}.yaml | shyaml get-value resources.${index}.name`
+    model_path=`cat inference_server/assets/conf/model/${model_config}.yaml | shyaml get-value resources.${index}.model_path`
+    template_path = `cat inference_server/assets/conf/model/${model_config}.yaml | shyaml get-value resources.${index}.template_path`
+
+    echo "[$((${index} + 1))/$((${model_count} + 1))] ${model_name}"
+
+    if [["rotate detection recognition"=~"${model_name}"*]];
+    then
+        echo -e "continue ${model_name} for prevent duplication\r\n"
+        continue
+    fi
+
+    if test -f inference_server/${model_path}; then
+        echo -e "Already exist\r\n"
+    else
+        path_depth=`echo ${model_path} | tr -cd '/' | wc -m`
+        model_path_s3=`echo ${model_path} | cut -d '/' -f 1-${path_depth}`
+        
+        aws s3 cp s3://lomin-model-repository/textscope/${model_path} inference_server/${model_path_s3}/ --recursive
+    fi
+
+    if [[ -v template_path ]];
+    then
+        cp -r /home/lomin/Templates/${BSN_CODE} inference_server/e2e_inference/assets    
+    fi
+        
+done
+########## 2. Download Model File End    ##########
+
+########## 3. Docker Build Start    ##########
+# docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --parallel
+########## 3. Docker Build End      ##########
+set -x
---
+++ circleci/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PATH="$HOME/.local/bin:$PATH"
+
+########## 1. Delete Docker Container & Volume and Network Start ##########
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml config | 
+shyaml keys services | { 
+    while read services; 
+    do 
+    docker stop `textscope-$services`; 
+    docker rm `textscope-$services`; 
+    done; 
+}|| true
+
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml config | 
+shyaml keys networks | { 
+    while read network; 
+    do 
+    docker network inspect $network | 
+    grep Name | 
+    grep -v $network | 
+    cut -d '"' -f4 | { 
+        while read service; 
+        do 
+        docker stop  $service; 
+        docker rm $service; 
+        done; 
+    }; 
+    if docker network rm $network ; then
+        echo "$network network rm successful"
+    else
+        echo "$network There is no container connected to the docker network"
+    fi
+    done; 
+}|| true
+
+# docker volume rm $(docker volume ls --filter name=${PWD##*/}_ --format "{{.Name}}") || true
+########## 1. Delete Docker Container & Volume and Network End   ##########
+
+
+########## 2. Docker Build Start    ##########
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build wrapper web serving pp
+########## 2. Docker Build End      ##########
