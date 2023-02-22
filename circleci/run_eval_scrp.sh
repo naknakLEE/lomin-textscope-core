9a33f78ba428267fc2ecada138c9df7ffbeea00e
ilwoo-lomin <ilwoo.park@lomin.ai>
2023-02-22 17:44:16 +0900

    fix eval scrp

--- circleci/run_eval_scrp.sh
+++ circleci/run_eval_scrp.sh
@@ -3,27 +3,27 @@
 
 cd /home/lomin/Circleci/textscope-client/textscope-evaluator
 
-for dir in $(ls /home/lomin/Circleci/sample/${BSN_CODE}); do
-    echo $dir
+for dir in $(ls /home/lomin/Circleci/sample/$BSN_CODE); do
+    echo "Sample dir: $dir"
     python3 tools/textscope_eval/textscope_requester.py \
         --textscope-url http://localhost:9100 \
-        --input-dir /home/lomin/Circleci/sample/bsn-2211-kbcard/ID-RRC\
-        --output-dir /home/lomin/Circleci/result/bsn-2211-kbcard/ID-RRC\
+        --input-dir /home/lomin/Circleci/sample/$BSN_CODE/$dir \
+        --output-dir /home/lomin/Circleci/result/$BSN_CODE/$dir \
         --endpoint-type kv \
         --doc-type KBC3-01 \
         --business-code kbc3 \
         --textscope-eval-config ./tools/textscope_eval/textscope_eval_config_kbc3.json &&
 
     python3 -m tools.textscope_eval.textscope_exporter \
-        --input-dir /home/lomin/Circleci/result/bsn-2211-kbcard/ID-RRC/inference_outputs \
-        --output-dir /home/lomin/Circleci/result/bsn-2211-kbcard/ID-RRC/excel \
+        --input-dir /home/lomin/Circleci/result/$BSN_CODE/$dir/inference_outputs \
+        --output-dir /home/lomin/Circleci/result/$BSN_CODE/$dir/excel \
         --business-code kbc3 \
         --textscope-eval-config ./tools/textscope_eval/textscope_eval_config_kbc3.json &&
 
     python3 tools/textscope_eval/textscope_evaluator.py \
-        --textscope-output-xlsx /home/lomin/Circleci/result/bsn-2211-kbcard/ID-RRC/excel/kv_result.xlsx \
-        --gt-xlsx /home/lomin/Circleci/gt/bsn-2211-kbcard/ID-RRC/ID-RRC_gt.xlsx \
-        --output-dir /home/lomin/Circleci/result/bsn-2211-kbcard/ID-RRC/ \
+        --textscope-output-xlsx /home/lomin/Circleci/result/$BSN_CODE/$dir/excel/kv_result.xlsx \
+        --gt-xlsx /home/lomin/Circleci/gt/$BSN_CODE/$dir/${dir}_gt.xlsx \
+        --output-dir /home/lomin/Circleci/result/$BSN_CODE/$dir/ \
         --business-code kbc3 \
         --textscope-eval-config ./tools/textscope_eval/textscope_eval_config_kbc3.json
 done
