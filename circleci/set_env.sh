a85e720208870852dfe82ae1c3b3ae37bb776f85
ilwoo-lomin <ilwoo.park@lomin.ai>
2023-02-23 17:53:45 +0900

    group add

--- circleci/set_env.sh
+++ circleci/set_env.sh
@@ -46,7 +46,7 @@ sed -i "s/textscope-/${BSN_CODE}-/g" .env
 for Dockerfile in $(ls docker/base); do
     `cat docker/base/$Dockerfile | grep -A2 -B2 groupadd | grep useradd > /dev/null`
     if [ $? -ne 1 ]; then
-        sed -i'' -r -e "/useradd/a\RUN groupadd -r circleci -g 1001 && useradd -m -u 1001 -r -g circleci -s /sbin/nologin -c \"Circleci user\" circleci" docker/base/$Dockerfile
+        sed -i'' -r -e "/useradd/a\RUN groupadd -r circleci -g 1001 && useradd -m -u 1001 -r -g circleci -s /sbin/nologin -c \"Circleci user\" circleci && gpasswd -a textscope circleci" docker/base/$Dockerfile
         echo "$Dockerfile add user & group 1001"
     else 
         echo "$Dockerfile is not contain add user & group"
