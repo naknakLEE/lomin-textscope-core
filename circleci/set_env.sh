8eaf5fb755e2cda611c8a7af5eb3a0aba017dcfa
ilwoo-lomin <ilwoo.park@lomin.ai>
2023-02-23 13:36:38 +0900

    env file 변경

--- circleci/set_env.sh
+++ circleci/set_env.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# docker-compose 파일 변경(컨테이너이름 변경 및 network 변경을 위한 작업)
-
 BSN_CODE=$1
 
 PATH="$HOME/.local/bin:$PATH"
 
 # 1. env 파일 변경
 sed -i "s/TEXTSCOPE_BASE_IMAGE_VERSION=0.1.3/TEXTSCOPE_BASE_IMAGE_VERSION=$BSN_CODE/" .env
-echo -e "BSN_CODE=$BSN_CODE" | tee -a .env
+sed -i '/^BSN_CODE=/s/=.*/='$BSN_CODE'/' .env
 
 # 2. assets config & inference_server config 파일 변경
 old_serving_container_name=`yq e '.services.serving.container_name' docker-compose.yml`
